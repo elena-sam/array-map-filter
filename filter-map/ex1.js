de8a35ce329e1db77d36a02dd7080a76284e3475
Emilie Huet <hemilie94@gmail.com>
2018-11-08 11:47:19 +0100

    solve filter-map1

--- filter-map/ex1.js
+++ filter-map/ex1.js
@@ -23,6 +23,7 @@ Sortie attendue:
 */
 
 function getJedisNames(characters) {
-}
+  return characters.filter(char => char.side === 'light').map(char => char.name)
+};
 
 module.exports = getJedisNames;
